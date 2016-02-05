c40826c46272895422b8ce4c479233f43ade0986
heibl <heibsta@gmx.net>
2016-02-05 17:00:42 +0100

    import OK until january 2016

---
+++ R/readOrnithoSingleFile.R
@@ -0,0 +1,61 @@
+## This code is part of the ornitho package
+## © C. Heibl 2015 (last update 2016-02-05)
+
+readOrnithoSingleFile <- function(file, attribute){
+  
+  cat("\nreading:", file)
+  
+  ## get column names as the appear in the file:
+  ## -------------------------------------------
+  x <- read.csv2(file = file, as.is = TRUE, sep = "\t", 
+                 fill = TRUE, 
+                 nrows = 1, quote = "", dec = ".", 
+                 encoding = "latin1")
+  x <- names(x)
+  
+  ## load the in-build column headers:
+  if (missing(attribute) ) data("attribute", envir = environment())
+  
+#   ## find column names that are not in-build
+#   ## add add them to the data frame:
+#   ## ------------------------------
+#   newCols <- setdiff(x, attribute$names)
+#   if (length(newCols) > 0) {
+#     cat("\nadd new columns:", paste(newCols, collapse = " "))
+#     newCols <- data.frame(names = newCols, alias = tolower(newCols), 
+#                           class = "NULL", 
+#                           order = nrow(attribute) + (1:length(newCols)))
+#     attribute <- rbind(attribute, newCols)
+#     attribute <- attribute[match(x, attribute$names), ]
+#   }
+  
+  ## find column names that are not present in the file
+  ## and delete them from the attribute table
+  ## ----------------------------------------
+  missCols <- setdiff(attribute$names, x)
+  if ( length(missCols) > 0 ) {
+#     cat("\ndelete deprecated columns:", 
+#         paste(missCols, collapse = " "))
+    fileCols <- attribute[!attribute$names %in% missCols, ]
+  }
+  
+  ## reorder attribute data frame according to file:
+  fileCols <- fileCols[match(x, fileCols$name), ]
+  
+  ## read and return ornitho data
+  obj <- read.csv2(file, as.is = TRUE, 
+                 sep = "\t", fill = TRUE,
+                 skip = 2, quote = "", dec = ".",
+                 col.names = fileCols$alias, 
+                 colClasses = fileCols$class,
+                 encoding = "latin1")
+  missCols <- attribute[attribute$names %in% missCols & attribute$class != 'NULL',
+                           "alias"]
+  if ( length(missCols) > 0 ) {
+    m <- matrix(nrow = nrow(obj), ncol = length(missCols))
+    m <- as.data.frame(m)
+    names(m) <- missCols
+    obj <- cbind(obj, m)
+  }
+  obj[match(attribute$alias, names(obj)), ]
+}
--- R/setAttributes.R
+++ R/setAttributes.R
@@ -1,26 +1,43 @@
+## This code is part of the ornitho package
+## © C. Heibl 2015 (last update 2015-11-25)
 
 setAttributes <- function(file){
+  
+  ## find set of column names:
+  ## ------------------------
+  x <- lapply(file, read.csv2, as.is = TRUE, 
+              sep = "\t", fill = TRUE,
+              nrows = 1, quote = "", dec = ".",
+              encoding = "latin1")
+  x <- lapply(x, function(z) names(z))
+  x <- unique(unlist(x))
     
-    x <- read.csv2(file = file, as.is = TRUE, sep = "\t", fill = TRUE, 
-                   nrows = 1, quote = "", dec = ".", encoding = "latin1")
-    x <- names(x)
-
-    data("attribute", envir = environment())
-    depCols <- setdiff(attribute$names, x)
-    newCols <- setdiff(x, attribute$names)
-
-    if (length(depCols) > 0) {
-        cat("delete deprecated columns:", paste(depCols, collapse = " "), 
-            "\n")
-        depCols <- attribute$names %in% depCols
-        attribute <- attribute[!depCols, ]
-    }
-    if (length(newCols) > 0) {
-        cat("add new columns:", paste(newCols, collapse = " "))
-        newCols <- data.frame(names = newCols, alias = tolower(newCols), 
-            class = "character",)
-        attribute <- rbind(attribute, newCols)
-        attribute <- attribute[match(x, attribute$names), ]
-    }
-    attribute
+  ## match current header with definition
+  ## ------------------------------------
+  data("attribute", envir = environment())
+  depCols <- setdiff(attribute$names, x)
+  newCols <- setdiff(x, attribute$names)
+  
+  ## delete deprecated columns
+  ## -------------------------
+  if ( length(depCols) > 0 ){
+    cat("delete deprecated columns:", 
+        paste(depCols, collapse = " "), "\n")
+    depCols <- attribute$names  %in% depCols
+    attribute <- attribute[!depCols, ] 
+  }
+    
+  ## add new columns
+  ## -------------------------
+  if ( length(newCols) > 0 ){
+    cat("add new columns:", 
+        paste(newCols, collapse = " "))
+    newCols <- data.frame(names = newCols,
+                          alias = tolower(newCols), 
+                          class = "character",
+                          order = nrow(attribute) + (1:length(newCols)))
+    attribute <- rbind(attribute, newCols)
+    attribute <- attribute[match(x, attribute$names), ]
+  }
+  attribute
 }
