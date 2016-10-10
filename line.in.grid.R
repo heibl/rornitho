573f4c3827fcfd0f44b74da50b3557f2062b4ec4
heibl <heibsta@gmx.net>
2016-10-10 20:43:54 +0200

    version 1.0-4

--- line.in.grid.R
+++
@@ -1,8 +0,0 @@
-line.in.polygon <- function(line, polygon){
-  
-  lxy <- line@lines[[1]]@Lines[[1]]@coords
-  lxy <- SpatialPoints(coords = lxy,
-                       proj4string = CRS(proj4string(polygon)))
-  id <- over(lxy, polygon)
-  !all(is.na(id$OBJECTID))
-}
